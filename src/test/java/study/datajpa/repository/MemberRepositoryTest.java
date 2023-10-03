3cc401f49434a658fe40614d2c64b320a04cd002
wodls1616 <wodls1616@gmail.com>
2023-10-03 22:21:18 +0900

    QueryHint는 내가 지정한 동작만 하도록 설정하는 것이다. 내가 select를 할경우 데이터가 변경될 경우 더티 체킹으로 인해서 데이터가 업데이트가 된다. 하지만 이를 방지 하기 위해서 @QueryHint로 업데이트 되지 않도록 막아준다. Lock과 관련된 기능은 책으로 사서 직접 공부하는게 좋다 내용이 너무 많기 떄문이다

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -284,6 +284,18 @@ class MemberRepositoryTest {
             System.out.println("memberTeam = " + member.getTeam());
         }
 
-        // then
+    }
+
+    @Test
+    public void queryHint() {
+        Member member1 = memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+
+        // when
+        Member findMember = memberRepository.findReadOnlyByUsername("member1");
+        findMember.setUsername("member2");
+
+        em.flush();
     }
 }
