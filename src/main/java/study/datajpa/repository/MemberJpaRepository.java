606eb57e4c1b9914709a95b4786fe116865cfb6c
wodls1616 <wodls1616@gmail.com>
2023-10-03 17:55:59 +0900

    NamedQuery를 사용하여 쿼리를 미리 생성하고 불러오기만 하면 된다. 이때 Spring JPA는 엔티티.메서드명과 같은 NamedQuery를 엔티티에서 먼저 찾고 없으면 해당 메서드의 쿼리를 만들어준다.

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -45,4 +45,9 @@ public class MemberJpaRepository {
                 .setParameter("username", username)
                 .setParameter("age",age).getResultList();
     }
+
+    public List<Member> findByUsername(String username){
+        return em.createNamedQuery("Member.findByUsername", Member.class)
+                .setParameter("username", username).getResultList();
+    }
 }
