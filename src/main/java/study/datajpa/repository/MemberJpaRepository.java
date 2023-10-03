3b64c6bef5583dcc56805930dcd50176797f798f
wodls1616 <wodls1616@gmail.com>
2023-10-03 22:50:32 +0900

    Member.class를 추가해서 반환되는 데이터 타입을 지정

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -52,7 +52,7 @@ public class MemberJpaRepository {
     }
 
     public List<Member> findByPage(int age, int offset, int limit) {
-        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc", Member.class)
                 .setParameter("age", age)
                 .setFirstResult(offset)
                 .setMaxResults(limit).getResultList();
